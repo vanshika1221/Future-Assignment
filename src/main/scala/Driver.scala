53df416166a907e719e1dde6d47dbe637f5d4cf1
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-04-12 14:00:10 +0530

    Session 5 Assignment 2 Added

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -4,7 +4,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 object Driver extends App {
-  private val csvFilePath = new StudentsData("src/main/scala/StudentsData.csv")
+  private val csvFilePath = new StudentsData("src/main/scala/StudentsDaata.csv")
   private val futureData = csvFilePath.csvFileReader()
 
   private val studentAverageData = csvFilePath.calculateStudentAverages(futureData)
@@ -12,15 +12,15 @@ object Driver extends App {
   private val gradesData = csvFilePath.calculateGrades(futureData)
 
   studentAverageData.onComplete {
-    case Failure(exception) => println(exception.getMessage)
+    case Failure(exception) => println(s"Error in calculating students average: ${exception.getMessage}")
     case Success(value) => println(s"Student Average Data: $value")
   }
   classAverageData.onComplete {
-    case Failure(exception) => println(exception.getMessage)
+    case Failure(exception) => println(s"Error in calculating class average: ${exception.getMessage}")
     case Success(value) => println(s"Class Average Data: $value")
   }
   gradesData.onComplete {
-    case Failure(exception) => println(exception.getMessage)
+    case Failure(exception) => println(s"Error in calculating grades: ${exception.getMessage}")
     case Success(value) => println(s"Grades Data: $value")
   }
 
